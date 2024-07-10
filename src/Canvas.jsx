bed29f44d594cb5628f2a9b01108feb172aaa927
J-Shun <ter0123540@gmail.com>
2024-07-10 23:58:02 +0800

    feat: 實作丟上圖片的功能

--- src/Canvas.jsx
+++ src/Canvas.jsx
@@ -40,5 +40,85 @@ export const Canvas = () => {
     };
   }, [canvasRef, contextRef, virtualCanvasRef, virtualContextRef]);
 
+  /**
+   * 圖片丟入 canvas
+   */
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const canvasContext = contextRef.current;
+
+    const virtualCanvas = virtualCanvasRef.current;
+    const virtualContext = virtualContextRef.current;
+
+    const dragAndDrop = ['dragenter', 'dragover', 'dragleave', 'drop'];
+
+    // 防止瀏覽器預設行為（如：開啟）
+    const preventDefaults = (e) => {
+      e.preventDefault();
+      e.stopPropagation();
+    };
+
+    // 處理拖放事件
+    const handleDrop = (e) => {
+      const dataTransfer = e.dataTransfer;
+      const file = dataTransfer.files[0];
+
+      const reader = new FileReader();
+      reader.onload = (event) => {
+        const img = new Image();
+        img.onload = () => {
+          // 計算圖片置中要放的位置
+          const widthCenter = canvas.width / 2 - img.width / 2;
+          const heightCenter = canvas.height / 2 - img.height / 2;
+
+          // 將圖片繪製到虛擬畫布上
+          virtualContext.drawImage(
+            img,
+            widthCenter,
+            heightCenter,
+            img.width,
+            img.height
+          );
+
+          // 將虛擬畫布上的圖片繪製到主畫布上
+          canvasContext.drawImage(
+            virtualCanvas,
+            0,
+            0,
+            canvas.width,
+            canvas.height,
+            0,
+            0,
+            canvas.width,
+            canvas.height
+          );
+        };
+        img.src = event.target.result;
+      };
+      reader.readAsDataURL(file);
+    };
+
+    // 加入事件監聽器
+    const addEventListeners = () => {
+      dragAndDrop.forEach((event) => {
+        canvas.addEventListener(event, preventDefaults, false);
+      });
+      canvas.addEventListener('drop', handleDrop, false);
+    };
+
+    // 移除事件監聽器
+    const removeEventListeners = () => {
+      dragAndDrop.forEach((event) => {
+        canvas.removeEventListener(event, preventDefaults, false);
+      });
+      canvas.removeEventListener('drop', handleDrop, false);
+    };
+
+    addEventListeners();
+
+    // 清理函數以解除事件監聽器
+    return () => removeEventListeners();
+  }, [canvasRef, contextRef, virtualCanvasRef, virtualContextRef]);
+
   return <canvas ref={canvasRef} style={{ display: 'block' }} />;
 };
